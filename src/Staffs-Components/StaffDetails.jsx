fcd80e0848ab50a1096e03d5261b6cadb81f32d2
karthikn1997 <karthikn951@gmail.com>
2024-11-18 14:03:36 +0530

    kk

--- src/Staffs-Components/StaffDetails.jsx
+++ src/Staffs-Components/StaffDetails.jsx
@@ -6,26 +6,37 @@ const StaffDetails = () => {
     {
       id: 1,
       photo: 'https://via.placeholder.com/150',
-      name: 'John Doe',
-      role: 'Manager',
+      name: 'Saravanan',
+      role: 'Managing Director',
     },
     {
       id: 2,
       photo: 'https://via.placeholder.com/150',
-      name: 'Jane Smith',
+      name: 'Naveen Kumar',
       role: 'Developer',
     },
     {
       id: 3,
       photo: 'https://via.placeholder.com/150',
-      name: 'Michael Johnson',
-      role: 'Designer',
+      name: 'Karthikeyan',
+      role: 'Developer',
+    },
+    {
+      id: 4,
+      photo: 'https://via.placeholder.com/150',
+      name: 'Aananthi',
+      role: 'Developer',
+    },
+    {
+      id: 5,
+      photo: 'https://via.placeholder.com/150',
+      name: 'Dheepthi',
+      role: 'Junior Developer',
     },
   ];
 
   return (
-    <div className="w-full min-h-screen p-4">
-      <h2 className="text-2xl font-semibold mb-4">Staff Details</h2>
+    <div className="w-full min-h-screen p-4 my-10">
       <div className="w-[80%] mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-6">
         {staffData.map((staff) => (
           <div
@@ -35,7 +46,7 @@ const StaffDetails = () => {
             <img
               src={staff.photo}
               alt={staff.name}
-              className="w-32 h-32 mx-auto mb-4"
+              className="w-40 h-46 mx-auto mb-4"
             />
             <h3 className="text-lg font-bold text-center">{staff.name}</h3>
             <p className="text-center text-gray-500">{staff.role}</p>
